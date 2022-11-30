38784c755265ad5a8d5b9c294431d1eff9cfda2a
Gary <77394810+Garyee23@users.noreply.github.com>
2022-12-01 02:23:10 +0900

    Add files via upload
    Add DB function by SQLite
    Codes are now made up good to see

--- stock.py
+++ stock.py
@@ -4,8 +4,12 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import urllib3
+import sqlite3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # 오류코드 삭제
+
+
+# ------------------------------------ 데이터 관련 ------------------------------------
 
 url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/" \
       "getStockPriceInfo?" \
@@ -32,6 +36,8 @@ df.drop(['mrktTotAmt'], axis=1, inplace=True)
 df = df.sort_index(ascending=False)
 df = df.reset_index(drop=True)
 
+# ------------------------------------ 기타 설정 ------------------------------------
+
 plt.rc('font', family='Malgun Gothic')  # plt 글꼴설정
 
 st.set_page_config(
@@ -52,32 +58,70 @@ def increment_counter(increment_value=0):
 def decrement_counter(decrement_value=0):
     st.session_state.count -= decrement_value
 
+# ------------------------------------ database ------------------------------------
+
+con = sqlite3.connect('stock.db')
+curr = con.cursor()
 
 money_df = pd.DataFrame({'구매금액': [], '구매수량': []})
 money_df_list = []
 
+# ------------------------------------ 변수들 ------------------------------------
+
+stock = df.loc[st.session_state.count]  # 현재 선택된 주식
+
+vsper = stock['fltRt']  # 전일대비등락률
+
+high = stock['hipr']  # 고가
+
+low = stock['lopr']  # 저가
+
+vsnum = stock['vs']  # 전일대비등락가격
+
+cur = stock['clpr']  # 현재가
+
+tod = stock['basDt']  # 현재 날짜
+
+chstock = df.astype({'basDt': 'int'})
+chstock = df.astype({'clpr': 'int'})
+chstock['price'] = chstock['clpr']
+
+seedmoney = 1000000  # 주식 시드머니(시작머니, 현재 100만원)
+
+# ------------------------------------ 수익계산관련 ------------------------------------
+
+query = curr.execute("SELECT * From user") #DB값 전체선택
+cols = [column[0] for column in query.description] #컬럼생성
+
+money_info = pd.DataFrame.from_records(data=query.fetchall(), columns=cols) #DB값으로 수익관련 DataFrame 생성
+st.dataframe(money_info)
+
+buysum = money_info['총매수금액'].sum()  # 총 매수금액
+st.write("총 매수금액 :", buysum)
+
+numsum = money_info['총매수량'].sum()  # 총 매수량
+st.write("총 매수량 :", numsum)
+
+
+if buysum == 0 or numsum == 0:
+    평단가 = 0
+else:
+    평단가 = int(buysum / numsum)
+    st.write("현재 평단가 :",평단가)
+
+매수가능금액 = seedmoney - buysum
+
+현재평가금액 = int(cur) * numsum
+
+손익 = 현재평가금액 - buysum
+
+수익률 = (손익 / buysum) * 100
+
+# ------------------------------------ streamlit ------------------------------------
+
 menu = st.sidebar.selectbox('MENU', options=['현재가', '로그인', '회원가입', '정보수정'])
 
 if menu == '현재가':
-    stock = df.loc[st.session_state.count]  # 현재 선택된 주식
-
-    vsper = stock['fltRt']  # 전일대비등락률
-
-    high = stock['hipr']  # 고가
-
-    low = stock['lopr']  # 저가
-
-    vsnum = stock['vs']
-
-    cur = stock['clpr']
-
-    tod = stock['basDt']
-
-    chstock = df.astype({'basDt': 'int'})
-    chstock = df.astype({'clpr': 'int'})
-    chstock['price'] = chstock['clpr']
-
-    total = 1000000
 
     if vsper[0] == '.':
         st.metric(label="현재가", value=cur + "원", delta='0' + vsper + "%")
@@ -116,19 +160,20 @@ if menu == '현재가':
         st.pyplot(plt)
 
     with col3:
-        Buy = st.number_input('매수할 수량을 입력하세요.', min_value=1, step=1)  # 매수 수량입력
+        Buynum = st.number_input('매수할 수량을 입력하세요.', min_value=1, step=1)  # 매수 수량입력
         Buybtn = st.button("매수하기")
+        buyprice = int(cur) * Buynum
+        st.write("구매가격은 :",buyprice)
         if Buybtn:
-            tbmoney = int(cur) * int(Buy)
-            st.write(tbmoney)
-            total = total - tbmoney
-            st.write(total)
-            add_df = {'구매금액': str(tbmoney), '구매수량': str(Buy)}
-            money_df_list.append(add_df)
-            money_df = pd.concat(money_df_list, ignore_index=True)
-            st.table(money_df)
+            if (매수가능금액 > 0):
+                curr.execute(f"INSERT INTO user(총매수금액, 총매수량)"
+                            f"VALUES({buyprice},{Buynum})")
+                con.commit()
+            else:
+                st.markdown("매수가능금액을 초과하였습니다.")
+        st.write("현재매수가능금액 :",매수가능금액)
 
+    
+    st.write(수익률)
 
-
-
-
+    
